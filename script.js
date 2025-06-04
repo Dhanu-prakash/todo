963d46148b3979d53ed921439f3fcad0fab032f7
Dhanu-prakash <dhanuprakash.edu@gmail.com>
2025-06-04 23:01:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,80 @@
-
-
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-let taskList = document.querySelector("#myList");
-//Variable for filtering 
+const task =( JSON.parse(localStorage.getItem("tasks"))) || [];
+let tasksList = document.querySelector("ul");
 let currentFilter = "all";
 
-//We need a html renderer;
-
-
 function render(){
-    taskList.innerText = "";
+    tasksList.innerHTML = "";
+    for(let i = 0 ; i < task.length; i++){
 
-    for(let i = 0 ;i<tasks.length;i++){
-        if((currentFilter==="done" && tasks[i].done === false) || (currentFilter === "notdone" && tasks[i].done === true))continue//skip that iteration if tasks if the filter is set to done and the tasks isn't done dont show up in the render.
+        if((currentFilter==="done" && task[i].done === false)||(currentFilter==="notdone" && task[i].done === true))continue;
 
+        
 
         let li = document.createElement("li");
-        li.textContent = tasks[i].text + "";
-        //lets render done button
-        let statusBtn = document.createElement("button");
-        statusBtn.classList.add("delBtn");
-        statusBtn.textContent = tasks[i].done === true ? "Done" : "Not Yet!";
-        statusBtn.style.backgroundColor = tasks[i].done === true ? "Green" : "Black";
-        statusBtn.style.color = tasks[i].done === true ? "black" : "white";
-        
-        statusBtn.addEventListener("click",function(){
-            tasks[i].done = tasks[i].done === false ? true : false;
-            localStorage.setItem("tasks",JSON.stringify(tasks));
-            render();
-        })
-        //lets render delete button
+        li.textContent = task[i].name;
 
         let delBtn = document.createElement("button");
-        delBtn.textContent = "DELETE";
-        delBtn.classList.add("delBtn");
-
+        delBtn.textContent = "Delete";
+        delBtn.classList.add("addBtn");
         delBtn.addEventListener("click",function(){
-            tasks.splice(i,1);
-            localStorage.setItem("tasks",JSON.stringify(tasks));
+            task.splice(i,1);
+            localStorage.setItem("tasks",JSON.stringify(task));
             render();
         })
-        li.appendChild(delBtn);
-        li.appendChild(statusBtn);
-        taskList.appendChild(li);
+
+        let filterStatus = document.createElement("button");
+        filterStatus.classList.add("filterStatus");
+        if(!task[i].done){
+            filterStatus.textContent = "Not Yet!"
+        filterStatus.style.backgroundColor="black";
+        filterStatus.style.color="white";
+        }
+        else{
+                filterStatus.textContent = "Yep Done!"
+        filterStatus.style.backgroundColor="green";
+        filterStatus.style.color="white";
+        }
         
+        filterStatus.addEventListener("click",function(){
+            task[i].done = task[i].done === false ? true : false;
+            filterStatus.style.backgroundColor= task[i].done === true ? "green":"black";
+            filterStatus.textContent = task[i].done === true ? "Yep Done!" : "Not Yet!";
+            
+        })
+
+
+        tasksList.appendChild(li);
+        li.appendChild(filterStatus);
+        li.appendChild(delBtn);
     }
 }
+render();
 
-//Add Filter Buttons
-document.querySelector("#filterAll").addEventListener("click",function(){
+document.querySelector(".addBtn").addEventListener("click",function(){
+    let text = document.querySelector(".input");
+    let textValue = text.value.trim();
+    if(textValue !== ""){
+        task.push({name:textValue,done:false});
+    localStorage.setItem("tasks",JSON.stringify(task));
+    }
+    
+    text.value = "";
+    render();
+})
+
+document.querySelector("#all").addEventListener("click",function(){
     currentFilter = "all";
     render();
 })
-document.querySelector("#filterDone").addEventListener("click",function(){
-    currentFilter = "done";
-    render();
-})
-document.querySelector("#filterAvailable").addEventListener("click",function(){
+
+document.querySelector("#notdone").addEventListener("click",function(){
     currentFilter = "notdone";
+    
     render();
 })
 
-//Add task
-document.querySelector("#addBtn").addEventListener("click",function(){
-    let input = document.querySelector("input");
-    let inputText = input.value.trim();
-    if(inputText!==""){
-        tasks.push({text:inputText, done:false});//Push done as false cuz the task will be undone if we push a new task to the ul
-        localStorage.setItem("tasks",JSON.stringify(tasks));
-        render();
-    }
-    input.value = "";
-})
+document.querySelector("#done").addEventListener("click",function(){
+    currentFilter = "done";
 
+    render();
+})
